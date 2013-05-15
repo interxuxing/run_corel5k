403ee164ef2fa7869396cb2c4f41af090da3b1b5
interxuxing <interxuxing.hust@gmail.com>
2013-05-15 20:15:47 +0900

    add a gEloss function
    gEloss: to calculate gradient for each image

---
+++ gEloss.c
@@ -0,0 +1,12 @@
+/*
+ * =============================================================
+ * gEloss.c 
+  
+ * takes two inputs, one (mxn) matrix M and one 
+ *                       (1x1) scalar k
+ * 
+ * 
+ * gEloss calculate gradient for the q-th image
+ * 
+ * =============================================================
+ */
